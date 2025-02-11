61b45e5a6952405866ebce79eb2c7d55760779f7
lucian-rus <rus.lucian04@gmail.com>
2025-02-11 20:48:37 +0200

    Feature: Start implementation of build system * update folder structure * add Cmake support * add build.py script - this is not yet used

---
+++ build.py
@@ -0,0 +1,6 @@
+import os
+
+# add params to build system
+
+os.system("cd build")
+os.system("cmake ..")
--- src/serverTypes.h
+++ src/serverTypes.h
@@ -4,18 +4,23 @@
 /* defines the actions that the user can take. will only use some for now */
 typedef enum {
     // drawing actions
-    Action_UpdateCoordinates = 0,
+    Action_UpdateCoordinates = 0u,
     Action_UpdateColor,
     Action_UpdateThickness,
+    Action_SetBackground,
+    // reserve more draw actions
 
+    // general actions
+    Action_Undo = 30u, // <- doing this server side cause it seems fun
+    Action_Redo,       // <- doing this server side cause it seems fun
+    Action_SetTheme,
     // moving actions
-    Action_ZoomIn = 100,
+    Action_ZoomIn,
     Action_ZoomOut, // might consider factor
     Action_ZoomPan, // might consider dir + amount
-} serverType_Action_t;
+    // reserve for future actions
 
-// define enum for each action defined above:
-// *update color
-// *update coordinates
+    Action_Exit = 255u,
+} serverType_Action_t;
 
 #endif
